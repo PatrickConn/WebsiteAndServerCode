1a3fc0fcf0fd49e7f45e305de2a130aad37e5919
PatrickConn <paddiconn@gmail.com>
2017-06-26 17:05:53 +0100

    pong unity
    A small unity pong game in C#

---
+++ Pong/Assets/Scripts/ContinuePlayingMusic.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ContinuePlayingMusic : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		DontDestroyOnLoad (gameObject);
+	}
+}
