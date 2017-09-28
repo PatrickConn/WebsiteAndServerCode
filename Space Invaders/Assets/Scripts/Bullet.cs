2c750293994b1ccaf3d9899489f84ea9d7ce5d31
PatrickConn <paddiconn@gmail.com>
2017-09-28 20:26:30 +0100

    Add files via upload

---
+++ Space Invaders/Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public float speed = 30;
+
+	private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+
+		rigidBody.velocity = Vector2.up * speed;
+	}
+	
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.tag == "Wall") {
+			Destroy(gameObject);
+		}
+
+	}
+
+	void OnBecomeInvisible(){
+
+		Destroy(gameObject);
+	}
+}
---
+++ Space Invaders/Assets/Scripts/SpaceShip.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceShip : MonoBehaviour {
+
+	public float speed = 30;
+
+	public GameObject theBullet;
+	void FixedUpdate(){
+		float horzMove = Input.GetAxisRaw ("Horizontal");
+
+		GetComponent<Rigidbody2D> ().velocity = new Vector2(horzMove, 0) * speed;
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Jump"))
+			{
+			Instantiate (theBullet, transform.position, Quaternion.identity);
+
+			// Play bullet fire sound
+			SoundManager.Instance.PlayOneShot (SoundManager.Instance.bulletFire);
+		
+
+		}
+
+
+	}
+}
